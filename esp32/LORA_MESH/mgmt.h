8cf1f63b3360c883d990bdd8dff3a13b89e27194
Lukas Stoeckli <lukas.stoeckli@unibas.ch>
2023-02-08 18:38:52 +0100

    feat(mgmt): two-hop-neighborhood for status table

--- esp32/LORA_MESH/mgmt.h
+++ esp32/LORA_MESH/mgmt.h
@@ -27,6 +27,8 @@ struct status_s {
 struct statust_entry_s {
   status_s state;
   unsigned long int received_on;
+  status_s neighbors[STATUST_SIZE];
+  int neighbor_cnt;
 };
 
 struct beacon_s {
@@ -41,6 +43,7 @@ struct beacon_s {
 
 struct statust_entry_s statust[STATUST_SIZE];
 int statust_cnt;
+int statust_rrb;
 
 
 // forward declaration
@@ -69,18 +72,37 @@ unsigned char* _mkRequest(unsigned char cmd, unsigned char* id=NULL)
 // fill buffer with status packet
 unsigned char* _mkStatus()
 {
-  static struct status_s status;
-  status.typ = 's';
-  status.id[0] = my_mac[4];
-  status.id[1] = my_mac[5];
-  status.voltage = axp.getBattVoltage()/1000;
-  status.feeds = feed_cnt;
-  status.entries = entry_cnt;
-  status.chunks = chunk_cnt;
+  static struct status_s status[(int) (120 - 11) / MGMT_STATUS_LEN];
+  // add self
+  status[0].typ = 's';
+  status[0].id[0] = my_mac[4];
+  status[0].id[1] = my_mac[5];
+  status[0].voltage = axp.getBattVoltage()/1000;
+  status[0].feeds = feed_cnt;
+  status[0].entries = entry_cnt;
+  status[0].chunks = chunk_cnt;
   int total = MyFS.totalBytes();
   int avail = total - MyFS.usedBytes();
-  status.free = avail / (total/100);
-  status.uptime = millis();
+  status[0].free = avail / (total/100);
+  status[0].uptime = millis();
+
+  // add neighbors
+  int maxEntries = (int) (120 - 11) / MGMT_STATUS_LEN;
+  int ndxNeighbor;
+  for (int i = 1; i < maxEntries; i++) {
+    if (i > statust_cnt) { break; }
+    ndxNeighbor = statust_rrb++ % statust_cnt;
+    status[i].typ = 's';
+    status[i].id[0] = statust[ndxNeighbor].state.id[0];
+    status[i].id[1] = statust[ndxNeighbor].state.id[1];
+    status[i].voltage = statust[ndxNeighbor].state.voltage;
+    status[i].feeds = statust[ndxNeighbor].state.feeds;
+    status[i].entries = statust[ndxNeighbor].state.entries;
+    status[i].chunks = statust[ndxNeighbor].state.chunks;
+    status[i].free = statust[ndxNeighbor].state.free;
+    status[i].uptime = statust[ndxNeighbor].state.uptime;
+  }
+
   return (unsigned char*) &status;
 }
 
@@ -101,6 +123,8 @@ void mgmt_rx(unsigned char *pkt, int len, unsigned char *aux)
 {
   pkt += DMX_LEN;
   len -= DMX_LEN;
+
+  Serial.printf("mgmt_rx received packet of len = %d\n", len);
 
   // receive beacon
   if (pkt[0] == 'b' && len == MGMT_BEACON_LEN) {
@@ -137,29 +161,79 @@ void mgmt_rx(unsigned char *pkt, int len, unsigned char *aux)
     mgmt_send_status();
     return;
   }
-  // receive status response
-  if (pkt[0] == 's' && len == MGMT_STATUS_LEN) {
-    struct status_s *status = (struct status_s*) calloc(1, MGMT_STATUS_LEN);
-    memcpy(status, pkt, MGMT_STATUS_LEN);
+  // receive status update
+  if (pkt[0] == 's' && len % MGMT_STATUS_LEN == 0) {
     unsigned long int received_on = millis();
-    Serial.println(String("mgmt_rx received status response from ") + to_hex(status->id, 2, 0));
+    int entries = len / MGMT_STATUS_LEN;
+    struct status_s *other = (struct status_s*) calloc(1, MGMT_STATUS_LEN);
+    memcpy(other, pkt, MGMT_STATUS_LEN);
+    Serial.println(String("mgmt_rx received status update from ") + to_hex(other->id, 2, 0));
     int ndx = -1;
+    // check if node is already in table
     for (int i = 0; i < statust_cnt; i++) {
-      if (!memcmp(status->id, statust[i].state.id, MGMT_ID_LEN)) {
+      if (!memcmp(other->id, statust[i].state.id, MGMT_ID_LEN)) {
         ndx = i;
       }
     }
+    // new node, check if table not full
     if (ndx == -1) {
-      ndx = statust_cnt++;
+      if (statust_cnt < STATUST_SIZE) {
+        ndx = statust_cnt++;
+      } else {
+	Serial.printf("%8sstatus table is full, skipping...\n", "");
+	free(other);
+	return;
+      }
     }
-    memcpy(statust[ndx].state.id, status->id, MGMT_ID_LEN);
-    statust[ndx].state.voltage = status->voltage;
-    statust[ndx].state.feeds = status->feeds;
-    statust[ndx].state.entries = status->entries;
-    statust[ndx].state.chunks = status->chunks;
-    statust[ndx].state.free = status->free;
-    statust[ndx].state.uptime = status->uptime;
+    memcpy(statust[ndx].state.id, other->id, MGMT_ID_LEN);
+    statust[ndx].state.voltage = other->voltage;
+    statust[ndx].state.feeds = other->feeds;
+    statust[ndx].state.entries = other->entries;
+    statust[ndx].state.chunks = other->chunks;
+    statust[ndx].state.free = other->free;
+    statust[ndx].state.uptime = other->uptime;
     statust[ndx].received_on = received_on;
+
+    pkt += MGMT_STATUS_LEN;
+
+    int ndxNeighbor;
+    for (int i = 1; i < entries; i++) {
+      // no next status
+      if (pkt[0] != 's') {
+        break;
+      }
+      struct status_s *neighbor = (struct status_s*) calloc(1, MGMT_STATUS_LEN);
+      memcpy(neighbor, pkt, MGMT_STATUS_LEN);
+      Serial.printf("%8slearned about %s\n", "", to_hex(neighbor->id, 2, 0));
+      pkt += MGMT_STATUS_LEN;
+      ndxNeighbor = -1;
+      // check if node is already in table
+      for (int i = 0; i < statust[ndx].neighbor_cnt; i++) {
+        if (!memcmp(neighbor->id, statust[ndx].neighbors[i].id, MGMT_ID_LEN)) {
+          ndxNeighbor = i;
+        }
+      }
+      // new node, check if table not full
+      if (ndxNeighbor == -1) {
+        if (statust[ndx].neighbor_cnt < STATUST_SIZE) {
+          ndxNeighbor = statust[ndx].neighbor_cnt++;
+	} else {
+	  Serial.printf("%8sneighbor table of %s is full, skipping...\n", "", to_hex(other->id, 2, 0));
+	  free(neighbor);
+	  continue;
+	}
+      }
+      memcpy(statust[ndx].neighbors[ndxNeighbor].id, neighbor->id, MGMT_ID_LEN);
+      statust[ndx].neighbors[ndxNeighbor].voltage = neighbor->voltage;
+      statust[ndx].neighbors[ndxNeighbor].feeds = neighbor->feeds;
+      statust[ndx].neighbors[ndxNeighbor].entries = neighbor->entries;
+      statust[ndx].neighbors[ndxNeighbor].chunks = neighbor->chunks;
+      statust[ndx].neighbors[ndxNeighbor].free = neighbor->free;
+      statust[ndx].neighbors[ndxNeighbor].uptime = neighbor->uptime;
+      free(neighbor);
+    }
+
+    free(other);
     return;
   }
   // receive reboot request
@@ -190,16 +264,18 @@ void mgmt_rx(unsigned char *pkt, int len, unsigned char *aux)
 void mgmt_send_status()
 {
   unsigned long int now = millis();
-  unsigned long int rand = random(1000);
+  unsigned long int rand = random(5000);
   while (true) { if (millis() - now > rand) { break; } }
-  io_enqueue(_mkStatus(), MGMT_STATUS_LEN, mgmt_dmx, NULL);
+  io_enqueue(_mkStatus(), ((int) ((120 - 11) / MGMT_STATUS_LEN)) * MGMT_STATUS_LEN, mgmt_dmx, NULL);
 }
 
 // print status table entry
-void _print_status(status_s* status, unsigned long int received_on = NULL)
+void _print_status(status_s* status, unsigned long int received_on = NULL, unsigned char* src = NULL)
 {
   // id
   Serial.printf("  %s", to_hex(status->id, MGMT_ID_LEN, 0));
+  // src
+  Serial.printf(" | %s", src == NULL ? "self" : to_hex(src, MGMT_ID_LEN, 0));
   // voltage
   Serial.printf(" | %6dV", status->voltage);
   // feeds, entries & chunks
@@ -222,8 +298,8 @@ void _print_status(status_s* status, unsigned long int received_on = NULL)
   int uh = (u / 1000 / 60 / 60) % 24;
   int ud = u / 1000 / 60 / 60 / 24;
   Serial.printf(" | %4dd %2dh %2dm %2ds", ud, uh, um ,us);
-  // self
-  if (received_on == NULL) { Serial.printf(" (self)"); }
+  // this node
+  Serial.printf(received_on == NULL ? " (this node)" : "");
   // newline
   Serial.printf("\n");
 }
@@ -232,9 +308,10 @@ void _print_status(status_s* status, unsigned long int received_on = NULL)
 void mgmt_print_statust()
 {
   // header
-  Serial.println("  id   | battery | feeds | entries | chunks | free | lastSeen | uptime");
+  Serial.println("  id   | src  | battery | feeds | entries | chunks | free | lastSeen | uptime");
   Serial.printf("  ");
   for (int i = 0; i < 4; i++) { Serial.printf("-"); } // id
+  for (int i = 0; i < 7; i++) { Serial.printf("-"); } // src
   for (int i = 0; i < 10; i++) { Serial.printf("-"); } // voltage
   for (int i = 0; i < 27; i++) { Serial.printf("-"); } // FEC
   for (int i = 0; i < 7; i++) { Serial.printf("-"); } // free
@@ -251,6 +328,10 @@ void mgmt_print_statust()
   // table entries
   for (int i = 0; i < statust_cnt; i++) {
     _print_status(&statust[i].state, statust[i].received_on);
+    // neighbors
+    for (int j = 0; j < statust[i].neighbor_cnt; j++) {
+      _print_status(&statust[i].neighbors[j], statust[i].received_on, statust[i].state.id);
+    }
   }
 }
 
